98bfd3f4012920252b66a4a87084794b858aefef
Pc-Prakhar <161551269+Pc-Prakhar@users.noreply.github.com>
2025-04-14 18:28:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-00db8dadb36c9815e"
+    ami = "ami-00a929b66ed6e0de6"
     instance_type = "t2.nano"
     tags = {
       Name = "Devjenkins-1"
